6edf46710047a4df676bb66ed8f957ec12b4ce56
Gaurav Agarwal <gauravagldeveloper28@gmail.com>
2026-01-26 16:12:06 +0530

    Update 2

--- n-term_of_sequence.cpp
+++ n-term_of_sequence.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"Gaurav Agarwal"<<endl;
-    cout<<"BETN1AI25099"<<endl;
-
+    
     int n;
     cout<<"Enter number of n-term:";
     cin>>n;
